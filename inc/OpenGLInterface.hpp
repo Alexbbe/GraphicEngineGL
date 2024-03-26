94b8db13410c195829db24a20f7a876c81a1d78b
Alexandru Balbae <balbaealexandru@gmail.com>
2024-03-26 08:11:16 +0200

    Starting to implement OpenGLInterface

--- inc/OpenGLInterface.hpp
+++ inc/OpenGLInterface.hpp
@@ -5,20 +5,25 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <vector>
+#include "SDLInterface.hpp"
 
 
 
 class OpenGLInterface
 {
 private:
+
     bool LoadVertexBuffers(std::vector<float> buffer);
+    bool LoadShaderFromFile();
+    bool CompileShader();
+    bool CreatePipelineProgram();
+    bool PreDraw();
+    bool Draw();
+    bool HandleInput();
+
+
+    SDLInterface sdlWorker;
     
-
-    
-
-
-
-
 
 public:
     OpenGLInterface();
